0a633bf1720713b699aeff5ffa84786d0cbf4fe8
Alexandr <jwchaganav@gmail.com>
2022-10-20 22:02:57 +0400

    end

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите четверть: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 1)
+    Console.WriteLine("Возможные варианты: X>0, У>0 ");
+else if (num == 2)
+    Console.WriteLine("Возможные варианты: X<0, У>0 ");
+else if (num == 3)
+    Console.WriteLine("Возможные варианты: X<0, У<0 ");
+else if (num == 4)
+    Console.WriteLine("Возможные варианты: X>0, У<0 ");
+else
+    Console.WriteLine("Такого варианта нет! ");
+
+
